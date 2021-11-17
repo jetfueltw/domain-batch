10b14b1aaf81f683d9dd105f7a7e0723e24f9f50
wily45454545 <wily45454545@gmail.com>
2021-11-17 16:14:16 +0800

    feat: move env

--- buy.py
+++ buy.py
@@ -1,10 +1,9 @@
 import argparse
-import os
-from dotenv import load_dotenv
 import requests
 import json
 from multipledispatch import dispatch
-from datetime import datetime 
+from datetime import datetime
+import env
 
 class bcolors:
     HEADER = '\033[95m'
@@ -28,12 +27,12 @@ def parse_cli_args():
 
 def api_agreement(domain):
     headers = {
-        'Authorization': 'sso-key ' + go_daddy_key + ':' + go_daddy_secret,
+        'Authorization': 'sso-key ' + env.go_daddy_key + ':' + env.go_daddy_secret,
         'X-Market-Id': 'en-US',
         'accept': 'application/json'
     }
 
-    r = requests.get(go_daddy_api +'/v1/domains/agreements?tlds='+ domain +'&privacy=false', headers=headers)
+    r = requests.get(env.go_daddy_api +'/v1/domains/agreements?tlds='+ domain +'&privacy=false', headers=headers)
 
     timestamp = datetime.strptime(r.headers['Date'], "%a, %d %b %Y %X %Z").isoformat()
 
@@ -46,7 +45,7 @@ def api_agreement(domain):
 @dispatch(str)
 def domain_purchase(domain):
     headers = {
-        'Authorization': 'sso-key ' + go_daddy_key + ':' + go_daddy_secret,
+        'Authorization': 'sso-key ' + env.go_daddy_key + ':' + env.go_daddy_secret,
         'Content-Type': 'application/json',
         'accept': 'application/json'
     }
@@ -55,16 +54,16 @@ def domain_purchase(domain):
 
     default_info = {
             "addressMailing": {
-                "address1": contact_address1,
-                "city": contact_city,
-                "country": contact_country,
-                "postalCode": contact_postalCode,
-                "state": contact_state
+                "address1": env.contact_address1,
+                "city": env.contact_city,
+                "country": env.contact_country,
+                "postalCode": env.contact_postalCode,
+                "state": env.contact_state
             },
-            "email": contact_email,
-            "nameFirst": contact_nameFirst,
-            "nameLast": contact_nameLast,
-            "phone": contact_phone
+            "email": env.contact_email,
+            "nameFirst": env.contact_nameFirst,
+            "nameLast": env.contact_nameLast,
+            "phone": env.contact_phone
     }
 
     params = {
@@ -74,13 +73,13 @@ def domain_purchase(domain):
         "contactRegistrant": default_info,
         "contactTech": default_info,
         "domain": domain,
-        "nameServers": name_server,
+        "nameServers": env.name_server,
         "period": 1,
         "privacy": False,
         "renewAuto": True
     }
 
-    r = requests.post(go_daddy_api+'/v1/domains/purchase', data=json.dumps(params), headers=headers)
+    r = requests.post(env.go_daddy_api+'/v1/domains/purchase', data=json.dumps(params), headers=headers)
 
     if(r.status_code != 200):
         print(bcolors.FAIL + "[Fail] Message: " + r.json()['message'])
@@ -91,53 +90,11 @@ def domain_purchase(domain):
 def domain_purchase(domains):
     for item in domains:
         domain_purchase(item)
-    
-def init():
-    load_dotenv()
-
-    global go_daddy_api
-    go_daddy_api = os.getenv('GO_DADDY_API')
-
-    global go_daddy_key
-    go_daddy_key = os.getenv('GO_DADDY_KEY')
-
-    global go_daddy_secret
-    go_daddy_secret = os.getenv('GO_DADDY_SECRET')
-
-    global name_server
-    name_server = json.loads(os.getenv('NAME_SERVER'))
-
-    global contact_address1
-    contact_address1 = os.getenv('CONTACT_ADDRESS1')
-
-    global contact_city
-    contact_city = os.getenv('CONTACT_CITY')
-
-    global contact_country
-    contact_country = os.getenv('CONTACT_COUNTRY')
-
-    global contact_postalCode
-    contact_postalCode = os.getenv('CONTACT_POSTAL_CODE')
-
-    global contact_state
-    contact_state = os.getenv('CONTACT_STATE')
-
-    global contact_email
-    contact_email = os.getenv('CONTACT_EMAIL')
-
-    global contact_nameFirst
-    contact_nameFirst = os.getenv('CONTACT_NAME_FIRST')
-
-    global contact_nameLast
-    contact_nameLast = os.getenv('CONTACT_NAME_LAST')
-
-    global contact_phone
-    contact_phone = os.getenv('CONTACT_PHONE')
 
 def main():
     args = parse_cli_args()
 
-    init()
+    env.init()
 
     if(args.domain_name != None):
         domain_purchase(args.domain_name)
