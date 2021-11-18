fd15af8dbfc6495563d95591b1d892691c465686
wilychen <wily45454545@gmail.com>
2021-11-18 10:34:32 +0800

    feat:新增domain數量顯示

--- buy.py
+++ buy.py
@@ -44,8 +44,8 @@ def api_agreement(domain):
         'agreedBy': requests.get('https://api.ipify.org').text
     }
 
-@dispatch(str)
-def domain_purchase(domain):
+@dispatch(str, int, int)
+def domain_purchase(domain, total, index):
     headers = {
         'Authorization': f'sso-key {env.go_daddy_key}:{env.go_daddy_secret}',
         'Content-Type': 'application/json',
@@ -84,29 +84,36 @@ def domain_purchase(domain):
     r = requests.post(f'{env.go_daddy_api}/v1/domains/purchase', json=params, headers=headers)
 
     if(r.status_code != 200):
-        print(f'{bcolors.FAIL}[Fail] Message: ' + r.json()['message'])
+        print(f'{bcolors.ENDC}({index}/{total}){bcolors.FAIL}[Fail] Message: ' + r.json()['message'])
+        return False
     else:
-        print(f'{bcolors.OKGREEN}({domain}) Success')
+        print(f'{bcolors.ENDC}({index}/{total}){bcolors.OKGREEN}({domain}) Success')
+        return True
 
 @dispatch(tuple)
 def domain_purchase(domains):
+    total = len(domains)
+    index = 1
     for item in domains:
-        domain_purchase(item)
+        domain_purchase(item, total, index)
+        index+=1
 
 def main():
+    print('Start buy domains...')
     args = parse_cli_args()
 
     env.init()
 
     if(args.domain_name != None):
-        domain_purchase(args.domain_name)
+        domain_purchase(args.domain_name, 1, 1)
     if(args.file != None):
         f = open(args.file, 'r')
         data = tuple(f.read().split('\n')[:-1])
+        total = len(data)
+        print(f'{bcolors.HEADER}Have {bcolors.OKCYAN}{total} {bcolors.HEADER}domains')
         domain_purchase(data)
 
     print(f'{bcolors.OKGREEN}Done!')
 
 if __name__ == "__main__":
-    print('Start buy domains...')
     main()
